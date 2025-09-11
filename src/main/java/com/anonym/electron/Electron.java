2340f2c7c17f265f515240e8cd85e24148514abd
Th3Fr3ak <castroarnaud@gmail.com>
2025-09-11 16:01:12 +0200

    First Commit

---
+++ src/main/java/com/anonym/electron/Electron.java
@@ -0,0 +1,39 @@
+package com.anonym.electron;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+@Mod(Electron.MODID)
+public class Electron {
+    public static final String MODID = "electron";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public Electron(IEventBus modEventBus, ModContainer modContainer) {
+
+
+        NeoForge.EVENT_BUS.register(this);
+
+
+        modEventBus.addListener(this::addCreative);
+
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        LOGGER.info("HELLO from server starting");
+    }
+}
